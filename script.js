816208e4c89a63174b97fc08afa87b6d67a89bba
Ana Paulo dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-23 09:57:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var exec3 = "->Exercício 3: [App SaveCollege] Alunos aprovados"
+console.log(exec3);
+
+aluno[0].matematica >= 7 ? console.log(aluno[0].nome + ": Aprovado!") : console.log(aluno[0].nome + ": Reprovado!");
+aluno[1].matematica >= 7 ? console.log(aluno[1].nome + ": Aprovado!") : console.log(aluno[1].nome + ": Reprovado!");
+aluno[2].matematica >= 7 ? console.log(aluno[2].nome + ": Aprovado!") : console.log(aluno[2].nome + ": Reprovado!");
+aluno[3].matematica >= 7 ? console.log(aluno[3].nome + ": Aprovado!") : console.log(aluno[3].nome + ": Reprovado!");
+aluno[4].matematica >= 7 ? console.log(aluno[4].nome + ": Aprovado!") : console.log(aluno[4].nome + ": Reprovado!");
